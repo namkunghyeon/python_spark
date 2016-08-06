9ca9db448c1f1576ac1f9274d393120860502e1a
root <onekyosi@naver.com>
2016-08-06 12:43:12 -0400

    added raw data

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ logData = sc.textFile('/home/odroid/Downloads/spark-2.0.0/README.md').cache()
 numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()
 
+logData2 = sc.textFile('/home/odroid/Work/raw_data/ml-10M100K/ratings.dat')
+rating1 = logData.filter(lambda s: '1' in s).count()
+rating2 = logData.filter(lambda s: '2' in s).count()
+print("Lines with rating 1: %i, lines with rating 2: %i" % (rating1, rating2))
+
+
 print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
 print a
 print a.take(2)
