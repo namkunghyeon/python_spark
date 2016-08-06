65d69cf995cd3907beffb76cfdb4463f27cf8062
root <onekyosi@naver.com>
2016-08-06 12:30:41 -0400

    added test code

--- test.py
+++ test.py
@@ -3,8 +3,11 @@ sc = SparkContext("local","simple app")
 
 a=['1','4','3','5']
 a = sc.parallelize(a)
-numAs = a.filter(lambda s: '1' in s).count()
-numBs = a.filter(lambda s: '2' in s).count()
+
+logData = sc.textFile('/home/odroid/Downloads/spark-2.0.0/README.md').cache()
+
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
 
 print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
 print a
