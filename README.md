46739bfbadb45ac2ca6b065ba48ca4afabd2a048
root <onekyosi@naver.com>
2016-08-06 12:27:38 -0400

    added install

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # python_spark
 
+#Python Spark Install
+- https://roshansanthosh.wordpress.com/2016/02/23/apache-spark-pyspark-standalone-installation-on-ubuntu-14-04/
+
+
+
 #START
-##/home/odroid/Downloads/spark-2.0.0/bin/spark-submit test.py
+- home/odroid/Downloads/spark-2.0.0/bin/spark-submit test.py
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from pyspark import SparkContext
 sc = SparkContext("local","simple app")
 
-a=[1,4,3,5]
+a=['1','4','3','5']
 a = sc.parallelize(a)
+numAs = a.filter(lambda s: '1' in s).count()
+numBs = a.filter(lambda s: '2' in s).count()
+
+print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
 print a
 print a.take(2)
